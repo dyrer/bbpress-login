5b5c6684f1a3566181cc52849219cf2d1d5eae4d
Athanasiadis Evagelos <dyrer@hyperwebhost.net>
2015-11-29 16:13:35 +0200

    Installation checks

--- bbpress-login-register-links-on-forum-page.php
+++ bbpress-login-register-links-on-forum-page.php
@@ -2,11 +2,30 @@
 /**
  * Plugin Name: Bbpress Login Register Links On Forum Topic Pages
  * Plugin URI:  http://athanasiadis.me
- * Description: Add bbpress login link, register links, on forum pages or topic pages so users can use our forums more easier. 
+ * Description: Add bbpress login link, register links, on forum pages or topic pages so users can use our forums more easier.
  * Author:      Athanasiadis Evagelos
  * Author URI:  http://athanasiadis.me
  * Version:     0.1
  */
+
+register_activation_hook(__FILE__,'bbpress_login_install');
+
+function bbpress_login_install() {
+	//do Checks
+
+	if( version_compare(get_bloginfo('version'),'4.5','<'))
+			{
+		deactivate_plugins(basename(__FILE__)); //Deactivate plugin
+			}
+		if(!class_exists('bbPress'))
+			{
+			deactivate_plugins(plugin_basename(__FILE__));
+			wp_die( __('Sorry, you need to activate bbPress first.', 'bbpress_notify'));
+			}
+
+	}
+
+
 function bbpressLoginRegisterLinksOnForumPage()
 {
 	echo '<div class="bbpressloginlinks" style="float:right;padding-right:20px;">';
@@ -17,19 +36,19 @@ function bbpressLoginRegisterLinksOnForumPage()
 
 		$register_url = site_url( 'wp-login.php?action=register' );
 		echo " <a href='$register_url' style='margin-left:20px;'>".' Register '.'</a> ';
-		
+
 		$lost_password_url = site_url( 'wp-login.php?action=lostpassword' );
 		echo " <a href='$lost_password_url' style='margin-left:20px;'>".' Lost Password '.'</a> ';
 	}
-	else 
+	else
 	{
 		$logout_url = wp_logout_url( get_permalink() );
 		echo "<a href='$logout_url'>".' Log Out'.'</a> ';
 	}
 	echo '</div>'; // class of "bbpressloginlinks"
 }
-add_action('bbp_template_after_forums_loop','bbpressLoginRegisterLinksOnForumPage'); 
-add_action('bbp_template_before_pagination_loop','bbpressLoginRegisterLinksOnForumPage'); 
-add_action('bbp_template_after_single_forum','bbpressLoginRegisterLinksOnForumPage'); 
-add_action('bbp_template_before_forums_loop','bbpressLoginRegisterLinksOnForumPage'); 
+add_action('bbp_template_after_forums_loop','bbpressLoginRegisterLinksOnForumPage');
+add_action('bbp_template_before_pagination_loop','bbpressLoginRegisterLinksOnForumPage');
+add_action('bbp_template_after_single_forum','bbpressLoginRegisterLinksOnForumPage');
+add_action('bbp_template_before_forums_loop','bbpressLoginRegisterLinksOnForumPage');
 ?>
